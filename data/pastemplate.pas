b720a9bea516974bd4992f925938d9d0fbf4300f
benibela <benito@benibela.de>
2012-04-20 15:27:53 +0200

    new pascal template command ( {%COMPARE a <>/= b} .. {%END-COMPARE}, not called if, since it only supports a single comparison), allow nested repeats/compares
    --HG--
    branch : trunks

--- data/pastemplate.pas
+++ data/pastemplate.pas
@@ -5,6 +5,8 @@ unit pastemplate;
 
 interface
 
+{$define unittests}
+
 uses
   Classes, SysUtils, LCLProc;
 
@@ -19,8 +21,10 @@ type TSpecialCallBack = function (name: string): string;
 //**Creates several copy of the text between REPEAT and END-REPEAT while replacing x by a1 and y by a2 in the first copy, x by b1 and y by b2 in the second, ...@br@br
 //**@code({%REPEAT} .. {%END-REPEAT}) @br
 //**Removes the text between REPEAT and END-REPEAT (i.e. make exactly zero copies)@br@br
+//**@code({%COMPARE x =/<> y} .. {%END-COMPARE}) @br
+//**Only inserts the text if x=y or x<>y. Its just comparing the trimmed text @br@br
 //**@code({%SPECIAL:foobar}) @br
-//**Calls an external callback function with the value foobar
+//**Calls an external callback function with the value foobar@br@br
 //**}
 function convertTemplate(s: string; special: TSpecialCallBack): string;
 
@@ -53,8 +57,36 @@ begin
   end;
 end;
 
-function convertTemplate(s: string; special: TSpecialCallBack): string;
-var cmdDef, cmdContent: string;
+//Given a string like openBracket  .. openBracket  ... closingBracket closingBracket closingBracket closingBracket , this will return everything between
+//the string start and the second last closingBracket (it assumes one bracket is already opened, so 3 open vs. 4 closing => second last).
+//If updateText, it will replace text with everything after that closingBracket. (always excluding the bracket itself)
+function strSplitGetBracketLike(const openBracket, closingBracket: string; var text: string; updateText: boolean): string;
+var pos: integer;
+  opened: Integer;
+begin
+  opened := 1;
+  while (pos < length(text)) and (opened >= 1) do begin
+    if strlcomp(@text[pos], @openBracket[1], length(openBracket)) = 0 then begin
+      opened += 1;
+      pos += length(openBracket);
+    end else if strlcomp(@text[pos], @closingBracket[1], length(closingBracket)) = 0 then begin
+      opened -= 1;
+      pos += length(closingBracket);
+    end else pos+=1;
+  end;
+  if opened < 1 then begin
+    pos-=1;
+    result := copy(text, 1, pos - length(closingBracket));
+    if updateText then delete(text, 1, pos);
+  end else begin
+    result := text;
+    if updateText then text := '';
+  end;
+end;
+
+//expand one level
+function convertSingleTemplate(s: string; special: TSpecialCallBack): string;
+var cmdDef, cmdName, compareOp: string;
     replace, temparray: TStringArray;
     repwith: array of TStringArray;
     temp: String;
@@ -66,12 +98,12 @@ begin
    result+=GetPart([], ['{%'], s);
    if s = '' then break;
    cmdDef := GetPart([], ['}'], s);
+   delete(s,1,1);
    delete(cmdDef, 1,2);
    if pos(' ', cmdDef) = 0 then begin
      if copy(UpperCase(cmdDef), 1, length('SPECIAL:')) = 'SPECIAL:' then begin
        //Handle callback command: {%SPECIAL:foobar}
        result+=special(copy(cmdDef, length('SPECIAL:')+1,length(cmdDef)));
-       delete(s,1,1);
      end else if UpperCase(cmdDef) = 'REPEAT' then begin
        //Handle remove/empty foreach command: {%REPEAT}...{%END-REPEAT}
        //no args => skip
@@ -79,7 +111,19 @@ begin
      end else
         raise Exception.Create('Unknown Command: '+cmdDef);
    end else begin
-      if UpperCase(GetPart([], [' '], cmdDef)) <> 'REPEAT' then
+      cmdName := UpperCase(GetPart([], [' '], cmdDef));
+      if cmdName = 'COMPARE' then begin
+        if pos('<>', cmdDef) > 0 then compareOp := '<>'
+        else if pos('=', cmdDef) > 0 then compareOp := '='
+        else raise Exception.Create('No comparison operator in '+cmdDef);
+        temp := GetPart([], [compareOp], cmdDef, true);
+        delete(cmdDef, 1, length(compareOp));
+        temp2:=strSplitGetBracketLike('{%COMPARE ', '{%END-COMPARE}', s, true);
+
+        if (trim(temp) = trim(cmdDef)) = (compareOp = '=') then result += temp2;
+        continue;
+      end;
+      if cmdName <> 'REPEAT' then
         raise Exception.Create('Unknown Command: '+cmdDef);
       cmdDef:=TrimLeft(cmdDef);
       if cmdDef[1] <> '(' then begin
@@ -114,8 +158,7 @@ begin
           repwith[i,j]:=trim(repwith[i,j]);
       end;
 
-      temp:=GetPart(['}'], ['{%END-REPEAT}'], s, true);
-      delete(s, 1, length('{%END-REPEAT}'));
+      temp:=strSplitGetBracketLike('{%REPEAT', '{%END-REPEAT}', s, true);
 
 //      writeln(stderr, temp);
       for i:=0 to high(repwith) do begin
@@ -130,6 +173,54 @@ begin
 
  end;
 end;
+
+function convertTemplate(s: string; special: TSpecialCallBack): string;
+begin
+  while true do begin
+    if pos('{%', s) <= 0 then exit(s);
+    s := convertSingleTemplate(s, special);  //handle nesting by expanding the template one level (todo: optimize)
+  end;
+end;
+
+{$ifdef unittests}
+
+var tests: array[1..24] of array[1..2] of string = (
+ ('abcdef'#13'ghi', 'abcdef'#13'ghi')
+,('foo{%COMPARE abc = abc}::{%END-COMPARE}bar', 'foo::bar')
+,('foo{%COMPARE abc <> abc}::{%END-COMPARE}bar', 'foobar')
+,('foo{%COMPARE abc = abcd}::{%END-COMPARE}bar', 'foobar')
+,('foo{%COMPARE abc <> abcd}::{%END-COMPARE}bar', 'foo::bar')
+,('foo{%COMPARE abc=abc }::{%END-COMPARE}bar', 'foo::bar')
+,('foo{%COMPARE abc<>abc }::{%END-COMPARE}bar', 'foobar')
+,('foo{%COMPARE abc=abcd }::{%END-COMPARE}bar', 'foobar')
+,('foo{%COMPARE abc<>abcd }::{%END-COMPARE}bar', 'foo::bar')
+,('foo{%REPEAT}removedcrap{%END-REPEAT}bar', 'foobar')
+,('foo{%REPEAT a, [x]}abc{%END-REPEAT}bar', 'fooxbcbar')
+,('foo{%REPEAT a, [x,y,z]}abc{%END-REPEAT}bar', 'fooxbcybczbcbar')
+,('foo{%REPEAT (a,c), [(A,C),(A,D),(A,E)]}abc{%END-REPEAT}bar', 'fooAbCAbDAbEbar')
+,('foo{%REPEAT (a, c), [(A,C)]}abc{%END-REPEAT}bar', 'fooAbCbar')
+,('foo{%REPEAT _a, [x,y,z]}:{%COMPARE _a = y}_abc{%END-COMPARE}:{%END-REPEAT}bar', 'foo:::ybc:::bar')
+,('foo{%COMPARE abc = abc}{%COMPARE def=def}::{%END-COMPARE}{%END-COMPARE}bar', 'foo::bar')
+,('foo{%COMPARE abc = abc}{%COMPARE def<>def}::{%END-COMPARE}{%END-COMPARE}bar', 'foobar')
+,('foo{%COMPARE abc <> abc}{%COMPARE def<>def}::{%END-COMPARE}{%END-COMPARE}bar', 'foobar')
+,('foo{%COMPARE abc <> abc}{%COMPARE def<>def}::{%END-COMPARE}{%END-COMPARE}bar', 'foobar')
+,('foo{%REPEAT (a, c), [(A,C)]}{%REPEAT b, [B,X]}abc{%END-REPEAT}{%END-REPEAT}bar', 'fooABCAXCbar')
+,('foo{%REPEAT (a, c), [(A,C)]}{%REPEAT b, [B,X]}abc{%END-REPEAT}{%END-REPEAT}bar', 'fooABCAXCbar')
+,('foo{%REPEAT (_a, _b), [(x,l),(y,m),(z,n),(x,m),(y,n),(z,o)]}{%COMPARE _a = x}_a_bc{%END-COMPARE}{%END-REPEAT}bar', 'fooxlcxmcbar')
+,('foo{%REPEAT (_a, _b), [(x,l),(y,m),(z,n),(x,m),(y,n),(z,o)]}{%COMPARE _a = x}_a_bc{%END-COMPARE}{%END-REPEAT}bar', 'fooxlcxmcbar')
+,('foo{%REPEAT (_a, _b), [(x,l),(y,m),(z,n),(x,m),(y,n),(z,o)]}{%COMPARE _a = x}{%COMPARE _b = m}_a_bc{%END-COMPARE}{%END-COMPARE}{%END-REPEAT}bar', 'fooxmcbar')
+//,('foo{%REPEAT a, [x,y,z]}:{%COMPARE a = y}abc{%END-COMPARE}:{%END-REPEAT}bar', 'foo:::ybc:::bar')
+);
+var i:integer;
+initialization
+
+for i:=low(tests) to high(tests) do begin
+  if convertTemplate(convertTemplate(tests[i,1], nil), nil) <> tests[i,2] then
+    raise Exception.Create('Test ' +inttostr(i)+ ' failed: '+tests[i,1] + LineEnding + convertTemplate(convertTemplate(tests[i,1], nil),nil) + ' <> ' + tests[i,2]) ;
+end;
+
+
+{$endif}
 
 end.
 
