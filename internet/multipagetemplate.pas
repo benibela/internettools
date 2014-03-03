961d3685c935f302607173a7152ecf2a0bb51365
benibela <benito@benibela.de>
2014-03-03 01:00:50 +0100

    fix broken <page url="{$var}"> command in multipagetemplate for the first page
    --HG--
    branch : trunks

--- internet/multipagetemplate.pas
+++ internet/multipagetemplate.pas
@@ -652,7 +652,7 @@ begin
     if (pos('"', url) = 0) and (pos('{', url) = 0) and (pos('}', url) = 0) then cururl := url
     else if (url[1] = '{') and (url[length(url)] = '}') and (pos('$', url) > 0) and (trim(copy(url, 2, length(url)-2))[1] = '$') and
       reader.parser.variableChangeLog.hasVariable(trim(copy(url, pos('$', url)+1, length(url) - pos('$', url) - 1)), @tempvalue) then begin
-      tempvi := reader.parser.QueryEngine.evaluateXPath3('pxp:resolve-html(., $url)', tempvalue).getChild(1);
+      tempvi := reader.parser.QueryEngine.evaluateXPath3('pxp:resolve-html(., pxp:get("url"))', tempvalue).getChild(1);
       if tempvi.kind = pvkObject then begin
         cururl := tempvi.getProperty('url').toString;
         curmethod := tempvi.getProperty('method').toString;
