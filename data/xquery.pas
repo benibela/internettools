2f94483599ed8a993eb281d19c68887cdba7e8f8
benibela <benito@benibela.de>
2014-12-31 19:49:40 +0100

    implement fn:has-children, fn:innermost, fn:outermost
    (for easy points)

--- data/xquery.pas
+++ data/xquery.pas
@@ -6589,6 +6589,11 @@ fn.registerFunction('element-with-id', @xqFunctionId, ['($arg as xs:string*) as 
 fn3.registerFunction('head', @xqFunctionHead, ['($arg as item()*) as item()?']);
 fn3.registerFunction('tail', @xqFunctionTail, ['($arg as item()*) as item()*']);
 
+fn3.registerFunction('has-children', @xqFunctionHas_Children, ['() as xs:boolean', '($node as node()?) as xs:boolean']);
+fn3.registerInterpretedFunction('innermost', '($nodes as node()*) as node()*', '$nodes except $nodes/ancestor::node()', []);
+fn3.registerInterpretedFunction('outermost', '($nodes as node()*) as node()*', '$nodes[not(ancestor::node() intersect $nodes)]/.', []);
+
+
 fn3.registerFunction('function-lookup', @xqFunctionFunction_lookup, ['($name as xs:QName, $arity as xs:integer) as function(*)?']);
 fn3.registerFunction('function-name', @xqFunctionFunction_Name, ['($func as function(*)) as xs:QName?']);
 fn3.registerFunction('function-arity', @xqFunctionFunction_Arity, ['($func as function(*)) as xs:integer']);
