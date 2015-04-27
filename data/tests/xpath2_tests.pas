9f993ff5e28b07dfd161cbe742bb2f217c8502e5
benibela <benito@benibela.de>
2015-04-27 14:28:46 +0200

    fix tokenization: number cannot be followed by operator without whitespace

--- data/tests/xpath2_tests.pas
+++ data/tests/xpath2_tests.pas
@@ -127,7 +127,7 @@ begin
   t('''He said, "I don''''t like it."''', 'He said, "I don''t like it."', '');
 
   f('12 div3', 'XPST0003');
-  //f('12div 3', 'XPST0003');
+  f('12div 3', 'XPST0003');
   f('12 div-3', 'XPST0003');
 
                 //Variable tests
