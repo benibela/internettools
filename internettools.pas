56e5b1823c8edabdaeb5dea01ad52dabc8eb88ab
benibela <benito@benibela.de>
2017-11-04 15:31:54 +0100

    remove deprecated files

--- internettools.pas
+++ internettools.pas
@@ -4,12 +4,13 @@
 
 unit internettools;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
   bbutils, extendedhtmlparser, simpleinternet, internetaccess, simplehtmlparser, simplehtmltreeparser, simplexmlparser, xquery, 
   synapseinternetaccess, w32internetaccess, simplexmltreeparserfpdom, xquery_json, mockinternetaccess, xquery__regex, xquery__parse, 
-  xquery_module_math, xquery__functions, LazarusPackageIntf;
+  xquery_module_math, xquery__functions, multipagetemplate, LazarusPackageIntf;
 
 implementation
 
