bd5073eeb633183bec8048b1728233feb23e0475
benibela <benito@benibela.de>
2020-04-05 18:50:02 +0200

    remove unused unit use

--- data/xquery.internals.common.pas
+++ data/xquery.internals.common.pas
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 interface
 
 uses
-  classes, contnrs, SysUtils, bbutils;
+  classes, SysUtils, bbutils;
 
 type
   TXQBaseTypeInfo = object
