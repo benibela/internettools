328c4bda6afb0f9b4509cf0270dd91666c5a70e1
benibela <benito@benibela.de>
2021-08-02 23:32:24 +0200

    update for renamed random number generator

--- internettools.pas
+++ internettools.pas
@@ -10,7 +10,7 @@ interface
 uses
   bbutils, extendedhtmlparser, htmlInformation, simplehtmlparser, simplehtmltreeparser, simplexmlparser, simplexmltreeparserfpdom, xquery, 
   xquery.internals.collations, xquery.internals.common, xquery.internals.floathelpers, xquery.internals.lclexcerpt, 
-  xquery.internals.protectionbreakers, xquery.internals.rng, xquery.namespaces, xquery__functions, xquery__parse, xquery__regex, 
+  xquery.internals.protectionbreakers, bbrandomnumbergenerator, xquery.namespaces, xquery__functions, xquery__parse, xquery__regex, 
   xquery__serialization, xquery_json, xquery_module_binary, xquery_module_file, xquery_module_math, xquery_module_uca_icu, internetaccess, 
   mockinternetaccess, multipagetemplate, simpleinternet, synapseinternetaccess, w32internetaccess, internetaccess_inflater_paszlib, 
   LazarusPackageIntf;
