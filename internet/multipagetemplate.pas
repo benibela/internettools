809e3c27e2e035a8e82749bbdbb5299536565965
benibela <benito@benibela.de>
2022-03-18 19:28:56 +0100

    update to renamed methods

--- internet/multipagetemplate.pas
+++ internet/multipagetemplate.pas
@@ -1165,7 +1165,7 @@ procedure TTemplateAction.addChildFromTree(context: TTemplateLoadingContext; t: 
     oldpath := context.path;
     if href.contains('/') or href.contains('\') then begin
       href := href.Replace('\', '/', [rfReplaceAll]);
-      dir := href.unsafeViewTo(href.unsafeView.findLast('/')).ToString;
+      dir := href.pcharViewTo(href.pcharView.findLast('/')).ToString;
       if context.path = '' then context.path := dir
       else context.path := IncludeTrailingPathDelimiter(context.path) + dir;
     end;
