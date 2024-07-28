4f0b3700edbaa79ef647570eedbdce7b891dba31
benibela <benito@benibela.de>
2024-07-28 16:30:50 +0200

    add htmlformsutils to project

--- internettools.pas
+++ internettools.pas
@@ -8,17 +8,12 @@ unit internettools;
 interface
 
 uses
-  bbutils, extendedhtmlparser, htmlInformation, simplehtmlparser, 
-  simplehtmltreeparser, simplexmlparser, simplexmltreeparserfpdom, xquery, 
-  xquery.internals.collations, xquery.internals.common, 
-  xquery.internals.floathelpers, xquery.internals.lclexcerpt, 
-  xquery.internals.protectionbreakers, bbrandomnumbergenerator, 
-  xquery.namespaces, xquery__functions, xquery__parse, xquery__regex, 
-  xquery__serialization, xquery_json, xquery_module_binary, 
-  xquery_module_file, xquery_module_math, xquery_module_uca_icu, 
-  internetaccess, mockinternetaccess, multipagetemplate, simpleinternet, 
-  synapseinternetaccess, w32internetaccess, internetaccess_inflater_paszlib, 
-  fastjsonscanner, LazarusPackageIntf;
+  bbutils, extendedhtmlparser, htmlInformation, simplehtmlparser, simplehtmltreeparser, simplexmlparser, simplexmltreeparserfpdom, xquery, 
+  xquery.internals.collations, xquery.internals.common, xquery.internals.floathelpers, xquery.internals.lclexcerpt, 
+  xquery.internals.protectionbreakers, bbrandomnumbergenerator, xquery.namespaces, xquery__functions, xquery__parse, xquery__regex, 
+  xquery__serialization, xquery_json, xquery_module_binary, xquery_module_file, xquery_module_math, xquery_module_uca_icu, internetaccess, 
+  mockinternetaccess, multipagetemplate, simpleinternet, synapseinternetaccess, w32internetaccess, internetaccess_inflater_paszlib, 
+  fastjsonscanner, htmlformutils, LazarusPackageIntf;
 
 implementation
 
